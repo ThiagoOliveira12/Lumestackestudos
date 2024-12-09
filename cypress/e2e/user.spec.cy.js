a7d5126943522402fbdc88c023dc13b60318ce9d
Thiago de Araujo <thiagoaraujo@grupogelog.com.br>
2024-12-09 16:42:23 -0300

    adding my info click

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -6,18 +6,18 @@ describe('Orange HRM test', () => {
     passwordField: "[name='password']",
     loginButton: "[type='submit']",
     dashboradGrid: ".orangehrm-dashboard-grid",
-    wrongCredentials: "[role='alert']"
+    wrongCredentials: "[role='alert']",
+    myInfoButton: '[href="/web/index.php/pim/viewMyDetails"]'
   }
 
-
-
-  it('Login - Success', () => {
+  it.only('User Info Update - Success', () => {
     cy.visit('/auth/login')
     cy.get(selectorsList.usernameField).type(userData.userSuccess.userName)
     cy.get(selectorsList.passwordField).type(userData.userSuccess.password)
     cy.get("[type='submit']").click()
     cy.location('pathname').should('equal','/web/index.php/dashboard/index')
     cy.get(selectorsList.dashboradGrid)
+    cy.get(selectorsList.myInfoButton).click()
   }),
 
   it('Login - Fail', () => {
