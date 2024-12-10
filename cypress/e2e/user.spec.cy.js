86dedf7d65a2573b0235cc1071985a802366c21c
Thiago de Araujo <thiagoaraujo@grupogelog.com.br>
2024-12-10 15:08:10 -0300

    change user data

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -7,7 +7,11 @@ describe('Orange HRM test', () => {
     loginButton: "[type='submit']",
     dashboradGrid: ".orangehrm-dashboard-grid",
     wrongCredentials: "[role='alert']",
-    myInfoButton: '[href="/web/index.php/pim/viewMyDetails"]'
+    myInfoButton: '[href="/web/index.php/pim/viewMyDetails"]',
+    firstNameField: "[name='firstName']",
+    lastNameField: "[name='lastName']",
+    genericIdField: '.oxd-input--active',
+
   }
 
   it.only('User Info Update - Success', () => {
@@ -18,6 +22,9 @@ describe('Orange HRM test', () => {
     cy.location('pathname').should('equal','/web/index.php/dashboard/index')
     cy.get(selectorsList.dashboradGrid)
     cy.get(selectorsList.myInfoButton).click()
+    cy.get(selectorsList.firstNameField).clear().type('FirstNameTest')
+    cy.get(selectorsList.lastNameField).clear().type('LastNameTest')
+    cy.get(selectorsList.genericIdField).eq(4).clear().type('EmployeeTT')
   }),
 
   it('Login - Fail', () => {
